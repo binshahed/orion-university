7bf04bd7a264169b77d5f26695df120fffcff79d
Shahed <151946873+shahed-emsl@users.noreply.github.com>
2024-07-29 17:49:44 +0600

    enrol course

--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -6,6 +6,7 @@ import { StudentModel } from '../student/student.model'
 import { EnrolledCourseModel } from './enrolledCourse.model'
 import { TEnrolledCourse } from './enrolledCourse.interface'
 import mongoose, { Schema } from 'mongoose'
+import { SemesterRegistrationModel } from '../semesterRegistration/semesterRegistration.model'
 
 const createEnrolledCourse = async (
   userId: string,
@@ -14,7 +15,7 @@ const createEnrolledCourse = async (
   // check if course is already enrolled in the semester
 
   const { offeredCourse: offeredCourseId } = payload
-  const student = await StudentModel.findOne({ id: userId })
+  const student = await StudentModel.findOne({ id: userId }, { _id: 1 })
 
   if (!student) {
     throw new AppError(httpStatus.NOT_FOUND, 'Student not found')
@@ -28,6 +29,27 @@ const createEnrolledCourse = async (
   if (offeredCourse.maxCapacity <= 0) {
     throw new AppError(httpStatus.BAD_GATEWAY, 'Room is Full!')
   }
+  const semesterRegistration = await SemesterRegistrationModel.findById(
+    offeredCourse.semesterRegistration,
+  ).select('maxCredit')
+
+  console.log(semesterRegistration)
+
+  const enrolledCourse = await EnrolledCourseModel.aggregate([
+    {
+      $match: {
+        semesterRegistration: offeredCourse.semesterRegistration,
+        student: student._id,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        count: { $sum: 1 },
+      },
+    },
+  ])
+  console.log(enrolledCourse)
 
   const isStudentEnrolled = await EnrolledCourseModel.findOne({
     semesterRegistration: offeredCourse.semesterRegistration,
